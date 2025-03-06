cb2f7695b4b8ad10bb928b8f765425bb176a27b3
Satwik Kar <satwik.k.2000@gmail.com>
2025-03-06 13:09:30 +0530

    ADDED AXE AND PLAYER

--- Timber.cpp
+++ Timber.cpp
@@ -1,26 +1,29 @@
 // Created by Satwik on 3/4/25.
 #include <SFML/Graphics.hpp>
 #include <cstdlib> 
-#include <ctime>    
+#include <ctime>   
+#include <iostream> 
 using namespace sf;
 
 int main() {
     RenderWindow window(VideoMode(1920, 1080), "Timber");
 
    
-    Texture backgroundTexture, treeTexture, beeTexture, cloudTexture;
+    Texture backgroundTexture, treeTexture, beeTexture, cloudTexture,playerTexture,axeTexture;
     backgroundTexture.loadFromFile("graphics/background.png");    
     treeTexture.loadFromFile("graphics/tree.png");    
     beeTexture.loadFromFile("graphics/bee.png");
     cloudTexture.loadFromFile("graphics/cloud.png");
+    playerTexture.loadFromFile("graphics/player.png");
+    axeTexture.loadFromFile("graphics/axe.png");
 
-   
     Sprite backgroundSprite(backgroundTexture);
     Sprite treeSprite(treeTexture);
     Sprite beeSprite(beeTexture);
     Sprite cloudSprite1(cloudTexture), cloudSprite2(cloudTexture), cloudSprite3(cloudTexture), cloudSprite4(cloudTexture), cloudSprite5(cloudTexture), cloudSprite6(cloudTexture);
+    Sprite playerSprite(playerTexture);
+    Sprite axeSprite(axeTexture);
 
-   
     backgroundSprite.setPosition(0, 0);
     treeSprite.setPosition(900, 0);
     cloudSprite1.setPosition(100, 0);
@@ -32,7 +35,8 @@ int main() {
     
     beeSprite.setPosition(1920, 500);
 
-    
+    playerSprite.setPosition(1200,700);
+    axeSprite.setPosition(1100,800);
     std::srand(static_cast<unsigned>(std::time(nullptr)));  
 
     float beeSpeedX = -(200 + std::rand() % 100); 
@@ -50,14 +54,38 @@ int main() {
     float cloud6SpeedX = (2 + std::rand() % 100); 
     float cloud6SpeedY = (std::rand() % 100) - 90;   
     Clock clock;
-
+    float treeScaleY = 1;
+    //jump
+    float playerVelocityY = 0;  
+    bool isJumping = false;  
+    const float gravity = 980.0f;   
+    const float jumpStrength = -600.0f;  
+    const float groundY = 700;  
+    Keyboard k;
+     axeSprite.setOrigin(120,0);
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed)
+            if (event.type == Event::Closed){
                 window.close();
-        }
+            }
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Space && !isJumping) {
+                playerVelocityY = jumpStrength;   
+                isJumping = true;   
+            }
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::LControl) {
+            
+                axeSprite.setRotation(-40);
 
+            }
+            if (event.type == Event::KeyReleased && event.key.code == Keyboard::LControl) {
+            
+                axeSprite.setRotation(40);
+
+            }
+            
+        }
+         
        
         float deltaTime = clock.restart().asSeconds();
       
@@ -68,7 +96,7 @@ int main() {
         cloudSprite4.move(cloud4SpeedX*deltaTime,0);
         cloudSprite5.move(cloud5SpeedX*deltaTime,0);
         cloudSprite6.move(cloud6SpeedX*deltaTime,0);
-
+        treeSprite.setScale(1,treeScaleY);
         
         if (beeSprite.getPosition().x < -100) {
             float newY = 300 + (std::rand() % 400);
@@ -94,7 +122,16 @@ int main() {
         if(cloudSprite6.getPosition().x>window.getSize().x){
             cloudSprite6.setPosition(-300,0);
         }
+        playerVelocityY += gravity * deltaTime;
+        playerSprite.move(0, playerVelocityY * deltaTime);
 
+         
+        if (playerSprite.getPosition().y >= groundY) {
+            playerSprite.setPosition(1200, groundY);
+            isJumping = false;
+            playerVelocityY = 0;
+        }
+        axeSprite.setPosition(playerSprite.getPosition().x,playerSprite.getPosition().y+105);
         // Render
         window.clear();
         window.draw(backgroundSprite);
@@ -107,6 +144,8 @@ int main() {
         window.draw(cloudSprite5);
 
         window.draw(cloudSprite6);
+        window.draw(playerSprite);
+        window.draw(axeSprite);
 
         window.draw(beeSprite);
         window.display();
